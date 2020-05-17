cbcde4374d59fddbe5cad0bcd82bce3907d88851
Krystian Kalwik <ambitious156288@gmail.com>
2020-05-17 17:06:12 +0200

    change API to faster

--- src/Api/getPokemons.js
+++ src/Api/getPokemons.js
@@ -1,56 +1,62 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import styled from 'styled-components';
-import { Card, Button } from 'react-bootstrap';
+import { Card } from 'react-bootstrap';
 
 const StyledGallery = styled.div`
   display: grid;
-  grid-template-columns: repeat(auto-fill, minmax(285px, 1fr));
+  grid-template-columns: repeat(auto-fit, minmax(285px, 1fr));
   gap: 30px;
   margin: 100px 20px;
 `;
 
-const API_URL = 'https://api.pokemontcg.io';
-const VERSION = 'v1';
-const RESOURCE = 'cards';
+const API_URL = 'https://pokeapi.co/api';
+const VERSION = 'v2';
+const RESOURCE = 'pokemon';
 const API_ENDPOINT = `${API_URL}/${VERSION}/${RESOURCE}`;
 
 const getPokemons = () => {
-  const [page, setPage] = useState(1);
-  const [name, setName] = useState([]);
+  // const [pageSize, setPageSize] = useState(6);
+
+  const [currentPage, setCurrentPage] = useState(1);
+  const [pokemons, setPokemons] = useState([]);
 
   useEffect(() => {
     axios.get(API_ENDPOINT).then(res => {
-      // console.log(res.data.cards);
-      setName(res.data.cards);
-      setPage(2);
+      // console.log(res.data.results);
+      setPokemons(res.data.results);
+      setCurrentPage(2);
     });
-  }, [page]);
+  }, [currentPage]);
 
   return (
-    <StyledGallery>
-      {name.map(item => (
-        <div key={item.id}>
-          <Card style={{ width: '18rem' }}>
-            <Card.Img variant="top" src={item.imageUrl} />
-            <Card.Body>
-              <Card.Title>{item.name}</Card.Title>
-              <Card.Text>
-                <ul>
-                  <li>number: {item.number}</li>
-                  <li>hp: {item.hp}</li>
-
-                  <li>Supertype: Pokémon</li>
-                  <li>Subtype: Stage 2</li>
-                  <li>Rarity: Uncommon</li>
-                </ul>
-              </Card.Text>
-              <Button variant="primary">Go somewhere</Button>
-            </Card.Body>
-          </Card>
-        </div>
-      ))}
-    </StyledGallery>
+    <>
+      <StyledGallery>
+        {pokemons.map((item, number) => (
+          <div key={item.id}>
+            <img
+              src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${number +
+                1}.png`}
+              alt="img"
+            />
+            <Card border="dark" style={{ width: '18rem' }}>
+              <Card.Header>{`#${number + 1}`}</Card.Header>
+              <Card.Body>
+                <Card.Title>Dark Card Title</Card.Title>
+                <Card.Text>
+                  dsadsa
+                  <img
+                    src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${number +
+                      1}.png`}
+                    alt="img"
+                  />
+                </Card.Text>
+              </Card.Body>
+            </Card>
+          </div>
+        ))}
+      </StyledGallery>
+    </>
   );
 };
 
--- src/views/Root/Root.js
+++ src/views/Root/Root.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import GetPokemons from 'Api/GetPokemons';
-import Header from 'components/atoms/Header';
+import Header from 'components/Header';
 
 const Root = () => (
   <>
