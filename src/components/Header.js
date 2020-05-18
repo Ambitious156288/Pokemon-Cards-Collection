5da738ff09cccc3402c5dd57b1b4a28e5e685877
Krystian Kalwik <ambitious156288@gmail.com>
2020-05-19 00:07:28 +0200

    header styling

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,39 @@
 import React from 'react';
+import styled from 'styled-components';
+import { Navbar, Form, Nav, Button, FormControl } from 'react-bootstrap';
+import pokemon from 'assets/pokemon.png';
+
+const StyledImage = styled.img`
+  height: 35px;
+  width: 50px;
+`;
+
+const StyledButton = styled(Button)`
+  padding: 0 5px;
+`;
+
+const StyledNavbar = styled(Navbar)`
+  border-bottom: 1px solid green;
+`;
+
+const StyledH2 = styled.h2`
+  color: green;
+`;
 
 const Header = () => {
   return (
     <>
-      {/* <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/98/International_Pok%C3%A9mon_logo.svg/1200px-International_Pok%C3%A9mon_logo.svg.png" /> */}
-      <h1>Pokemon Card Collection</h1>
-      <p>search</p>
+      <StyledNavbar fixed="top" bg="dark">
+        <Nav className="mr-auto">
+          <StyledH2>Pokemon Cards Collection</StyledH2>
+        </Nav>
+        <Form inline>
+          <FormControl type="text" placeholder="search pokemons..." className="mr-sm-2" />
+          <StyledButton variant="success">
+            <StyledImage src={pokemon} alt="" />
+          </StyledButton>
+        </Form>
+      </StyledNavbar>
     </>
   );
 };
--- src/theme/Background.js
+++ src/theme/Background.js
@@ -1,11 +1,11 @@
 import { createGlobalStyle } from 'styled-components';
-import img from 'assets/img.jpg';
+import backgroundImage from 'assets/backgroundImage.png';
 
 const Background = createGlobalStyle`
     @import url('https://fonts.googleapis.com/css2?family=Roboto&display=swap');
     body{ 
         font-family: 'Roboto', sans-serif;
-        background: linear-gradient(rgba(255,255,255,.1), rgba(255,255,255,.1)), url(${img}) center;
+        background: linear-gradient(rgba(255,255,255,.1), rgba(255,255,255,.1)), url(${backgroundImage}) center;
     }
 `;
 
