f580915059d199e62cff60f05d3284c78c885654
Krystian Kalwik <ambitious156288@gmail.com>
2020-05-27 13:44:29 +0200

    Updates

---
+++ precache-manifest.cbe1ac48569f6b36148b80de473684fe.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "0470597c3d3d5de2a083",
+    "url": "/Pokemon-Cards-Collection/static/js/main.3a7b0c56.chunk.js"
+  },
+  {
+    "revision": "aee23bec33cb003fb443",
+    "url": "/Pokemon-Cards-Collection/static/js/runtime~main.dfc2fe59.js"
+  },
+  {
+    "revision": "3123f60fb319a8fc5446",
+    "url": "/Pokemon-Cards-Collection/static/css/2.ba6174bc.chunk.css"
+  },
+  {
+    "revision": "3123f60fb319a8fc5446",
+    "url": "/Pokemon-Cards-Collection/static/js/2.5e86dc34.chunk.js"
+  },
+  {
+    "revision": "396f90771b84b80229cd995204d6acde",
+    "url": "/Pokemon-Cards-Collection/static/media/backgroundImage.396f9077.png"
+  },
+  {
+    "revision": "4372dbbc152382b561504b8f3778a63e",
+    "url": "/Pokemon-Cards-Collection/index.html"
+  }
+];
